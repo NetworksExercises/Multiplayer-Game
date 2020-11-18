f0417aabac94eb829a52383f97f2d489911183b7
Aitor Simona Bouzas <simonaaitor@gmail.com>
2020-11-18 11:12:23 +0100

    added most of replication manager client functionality

--- Multiplayer_Game_Engine_-_Skeleton/Multiplayer Game/ReplicationManagerClient.cpp
+++ Multiplayer_Game_Engine_-_Skeleton/Multiplayer Game/ReplicationManagerClient.cpp
@@ -5,4 +5,42 @@
 
 void ReplicationManagerClient::read(const InputMemoryStream& packet)
 {
+	if (packet.GetSize() > 0)
+	{
+		uint32 network_Id;
+		ReplicationAction repAction;
+
+		packet.Read(network_Id);
+		packet.Read(repAction);
+
+		switch (repAction)
+		{
+		case ReplicationAction::None:
+			break;
+		case ReplicationAction::Create:
+			{
+			GameObject* go = App->modGameObject->Instantiate();
+			App->modLinkingContext->registerNetworkGameObjectWithNetworkId(go, network_Id);
+
+			// Deserialize go fields
+			}
+			break;
+		case ReplicationAction::Update:
+			{
+			GameObject* go = App->modLinkingContext->getNetworkGameObject(network_Id);
+
+			// Deserialize go fields
+			}
+			break;
+		case ReplicationAction::Destroy:
+			{
+			GameObject* go = App->modLinkingContext->getNetworkGameObject(network_Id);
+			App->modLinkingContext->unregisterNetworkGameObject(go);
+			App->modGameObject->Destroy(go);
+			}
+			break;
+		default:
+			break;
+		}
+	}
 }
